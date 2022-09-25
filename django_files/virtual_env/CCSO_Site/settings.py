d6ef06cfb0d734a8d8ffa27ebacd4a4969baf87a
kerolos90 <82464357+kerolos90@users.noreply.github.com>
2022-09-25 14:28:29 -0500

    Django files

--- django_files/virtual_env/CCSO_Site/settings.py
+++ django_files/virtual_env/CCSO_Site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'scheduling',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,10 @@ ROOT_URLCONF = 'CCSO_Site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates",
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- django_files/virtual_env/scheduling/views.py
+++ django_files/virtual_env/scheduling/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 # Create your views here.
 
+test_dic = {
+    "text": "Patrol challenges",
+    'range': range(10)
+
+}
 def patrol_schedule(request):
-    return HttpResponse("Patrol schedule!")
+    try:
+        return render(request,"scheduling/patrol_schedule.html", test_dic)
+    except:
+         raise Http404()
+
 
 def investigation_schedule(request):
     return HttpResponse("Investigation schedule")
