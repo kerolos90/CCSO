e698a5b90b62cdfb4b665962c979746af1c569c1
kerolos90 <82464357+kerolos90@users.noreply.github.com>
2022-10-22 10:22:35 -0500

    Front end updates

--- django_files/virtual_env/static/js/pages/calendar.init.js
+++ django_files/virtual_env/static/js/pages/calendar.init.js
@@ -5,53 +5,11 @@
 
   (CalendarPage.prototype.init = function () {
     var addEvent = $("#event-modal");
-    var modalTitle = $("#modal-title");
-    var formEvent = $("#form-event");
-    var selectedEvent = null;
-    var newEventData = null;
-    var forms = document.getElementsByClassName("needs-validation");
-    var selectedEvent = null;
-    var newEventData = null;
-    var eventObject = null;
     /* initialize the calendar */
-
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-    var Draggable = FullCalendarInteraction.Draggable;
-    var externalEventContainerEl = document.getElementById("external-events");
-    // init dragable
-    new Draggable(externalEventContainerEl, {
-      itemSelector: ".external-event",
-      eventData: function (eventEl) {
-        return {
-          title: eventEl.innerText,
-          className: $(eventEl).data("class"),
-        };
-      },
-    });
-    var defaultEvents = [];
-
-    var draggableEl = document.getElementById("external-events");
     var calendarEl = document.getElementById("calendar");
-
-    function addNewEvent(info) {
-      addEvent.modal("show");
-      formEvent.removeClass("was-validated");
-      formEvent[0].reset();
-
-      $("#event-title").val();
-      $("#event-category").val();
-      modalTitle.text("Add Event");
-      newEventData = info;
-    }
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
       plugins: ["bootstrap", "interaction", "dayGrid", "timeGrid"],
-      editable: true,
-      droppable: true,
-      selectable: true,
       defaultView: "dayGridMonth",
       themeSystem: "bootstrap",
       header: {
@@ -59,67 +17,55 @@
         center: "title",
         right: "prev,next",
       },
-      eventClick: function (info) {
-        addEvent.modal("show");
-        formEvent[0].reset();
-        selectedEvent = info.event;
-        $("#event-title").val(selectedEvent.title);
-        $("#event-category").val(selectedEvent.classNames[0]);
-        newEventData = null;
-        modalTitle.text("Edit Event");
-        newEventData = null;
-      },
-
+      eventSources: [
+        {
+          events: [
+            {
+              start: '2022-10-10',
+              rendering: 'background',
+            },
+            {
+              start: '2022-10-11',
+              rendering: 'background',
+            },
+            {
+              start: '2022-10-14',
+              rendering: 'background',
+            },
+            {
+              start: '2022-10-15',
+              rendering: 'background',
+            },
+            {
+              start: '2022-10-16',
+              rendering: 'background',
+            }
+          ],
+          rendering: 'background',
+          backgroundColor: '#7b3f00 '
+        },
+        {
+          events: [
+            {
+              start: '2022-10-12',
+              rendering: 'background',
+            },
+            {
+              start: '2022-10-13',
+              rendering: 'background',
+            }
+          ],
+          rendering: 'background',
+          backgroundColor: 'gold'
+        }
+      ],   
       dateClick: function (info) {
-        addNewEvent(info);
-      },
+        addEvent.modal("show");
 
-      events: defaultEvents,
+        console.log(info.dateStr);
+      },
     });
     calendar.render();
-
-    /*Add new event*/
-    // Form to add new event
-
-    $(formEvent).on("submit", function (ev) {
-      ev.preventDefault();
-      var inputs = $("#form-event :input");
-      var updatedTitle = $("#event-title").val();
-      var updatedCategory = $("#event-category").val();
-
-      // validation
-      if (forms[0].checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-        forms[0].classList.add("was-validated");
-      } else {
-        if (selectedEvent) {
-          selectedEvent.setProp("title", updatedTitle);
-          selectedEvent.setProp("classNames", [updatedCategory]);
-        } else {
-          var newEvent = {
-            title: updatedTitle,
-            start: newEventData.date,
-            allDay: newEventData.allDay,
-            className: updatedCategory,
-          };
-          calendar.addEvent(newEvent);
-        }
-        addEvent.modal("hide");
-      }
-    });
-
-    $("#btn-delete-event").on("click", function (e) {
-      if (selectedEvent) {
-        selectedEvent.remove();
-        selectedEvent = null;
-        addEvent.modal("hide");
-      }
-    });
-
-    $("#btn-new-event").on("click", function (e) {
-      addNewEvent({ date: new Date(), allDay: true });
-    });
   }),
     //init
      ($.CalendarPage = new CalendarPage()),
