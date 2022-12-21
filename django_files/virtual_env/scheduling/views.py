27f08a343209eaa63fa9a65b2504196162cbfab3
kerolos90 <82464357+kerolos90@users.noreply.github.com>
2022-12-21 14:45:58 -0600

    data

--- django_files/virtual_env/scheduling/views.py
+++ django_files/virtual_env/scheduling/views.py
@@ -9,34 +9,25 @@ from .forms import TimeOffRequestForm, EditScheduleForm
 
 beats = {
     "commandOne": "Shift Commander #1",
-    # "Shift Commander #2": "commandTwo",
-    # "North (1/2)": "north",
-    # "West (3/4)": "west",
-    # "Cover (4/5)": "cover",
-    # "East (5/6)": "east", 
-    # "South (7/8)": "south"
+    "commandTwo": "Shift Commander #2",
+    "north": "North (1/2)",
+    "west": "West (3/4)",
+    "cover": "Cover (4/5)",
+    "east": "East (5/6)", 
+    "south": "South (7/8)",
 }
 
-#deputies = ("501", "502", "503")
+hours = ["_first_four", "_second_four", "_third_four"]
 
 
 def patrol_schedule(request):
      #try:
     test_date = GoldDays.objects.get(date="2022-11-10")
-    if request.method == "POST":
-        editForm = EditScheduleForm(request.POST, instance=test_date)
-        if editForm.is_valid():
-            editForm.save()
-            entered_start_date = request.POST.get('commandOne_first_four', False)
-            print(entered_start_date)
-
     form = TimeOffRequestForm()
-    editScheduleForm = EditScheduleForm()
     return render(request, "scheduling/patrol_schedule.html", {
         "test_date": test_date,
         "form": form,
         "beats": beats,
-        "editScheduleForm": editScheduleForm,
     })
     # except:
     #      raise Http404()
@@ -44,10 +35,15 @@ def patrol_schedule(request):
 def edit_schedule(request, selected_beat ):
     test_date = GoldDays.objects.get(date="2022-11-10")
     editScheduleForm = EditScheduleForm(request.POST, instance=test_date)
+    beat = beats[selected_beat]
+    fields_to_edit = []
+    for section in hours :
+        fields_to_edit.append(editScheduleForm[selected_beat + section])
     return render(request, "scheduling/partials/test_partial.html",{
-        "beats": beats,
+        "beat": beat,
         "test_date": test_date,
         "selected_beat": selected_beat,
         "editScheduleForm": editScheduleForm,
+        "fields_to_edit": fields_to_edit,
 
     })
